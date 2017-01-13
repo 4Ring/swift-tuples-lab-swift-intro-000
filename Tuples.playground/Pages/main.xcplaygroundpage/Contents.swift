84016bf06c3d9b5dc922e104a2cae43dfdc9c532
4Ring <ukrainianslavic@gmail.com>
2017-01-12 23:01:03 -0800

    done

--- Tuples.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ Tuples.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -14,118 +14,41 @@
 /*: question1
  ### 1. Create a tuple to represent a person! The tuple should have a person's name in the first field and their age in the second field. Define the tuple as a variable named `person`.
  */
-// write your code here
-
-
-
-
-
-
-
-
+var person = (name: "BillyJean", age: 30)
 /*: question2
  ### 2. Print out "<Name> is <Age> years old" using the tuple you created in Question 1.
  */
-// write your code here
-
-
-
-
-
-
-
-
+print("\(person.name) is \(person.age) years old")
 /*: question3
  ### 3. It is the person's birthday! Increment the person's age by one year that you created in Question 1.
  */
-// write your code here
-
-
-
-
-
-
-
-
+person.age = 31
 /*: question4
  ### 4. Print out "Happy <Age> birthday, <Name>!" using the `person` variable.
  */
-// write your code here
-
-
-
-
-
-
-
-
+print("Happy \(person.age)st birthday, \(person.name)")
 /*: question5
  ### 5. Create another person variable called `someone` using _named_ fields for the name and age.
  */
-// write your code here
-
-
-
-
-
-
-
-
+var person2 = (name: "Hazel", age: 5)
 /*: question6
  ### 6. Print out "<Name> is <Age> years old" for the `someone` variable you created in Question 5.
  */
-// write your code here
-
-
-
-
-
-
-
+print("\(person2.name) is \(person2.age) years old")
 /*:
  ### 7. It is `someone`'s birthday! Increment `someone`'s age by one year.
  */
-// write your code here
-
-
-
-
-
-
-
+person2.age = 6
 /*:
  ### 8. Print out "Happy <Age> birthday, <Name>!" using the `someone` variable.
  */
-// write your code here
-
-
-
-
-
-
-
-
+print("Happy \(person2.age)th birthday, \(person2.name)!")
 /*:
  ### 9. Create a _constant_ tuple called `human`, with named fields for `name` and `age`.
  */
-// write your code here
-
-
-
-
-
-
-
-
+let human = (name: "Joe", age: 20)
 /*:
  ### 10. Can you change `human`'s age? Why or why not?
  */
-
-
-
-
-
-
-
-
+// You can't change human's age because it is declared as a constant. In order tochange his age you have to change the constant declartion to variable
 //: Check [here](https://github.com/learn-co-curriculum/swift-tuples-lab/blob/solution/Tuples.playground/Pages/solution.xcplaygroundpage/Contents.swift) for the solution.
